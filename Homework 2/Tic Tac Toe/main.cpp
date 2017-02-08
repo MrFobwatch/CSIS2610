1e04ef5aa3c0e09080e5ce3e6316037660fa1780
MrFobwatch <kcy0511@gmail.com>
2017-02-08 14:23:51 -0500

    Homework 2 Outline
    First Code outline for TIc Tac Toe

--- Homework 2/Tic Tac Toe/main.cpp
+++ Homework 2/Tic Tac Toe/main.cpp
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
  * Author: Kevin
@@ -12,13 +6,36 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 /*
- * 
+ * Tic Tac Toe Game
  */
+int board[3][3]; //Game board is stored without any labels or lines
+
+void displayBoard(){
+    for(int x=0; x<3; x++) {
+        for(int y=0; y<3; y++) {
+            
+        }
+    }
+}
+
+void placeCharacter(int x, int y){
+    board[x][y];
+    
+}
+
+void comPlayer(){
+    
+}
 int main(int argc, char** argv) {
+    char player = 'X';
+    char computer = 'O';
+    
+    
 
     return 0;
 }
