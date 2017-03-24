077cdc062274b5538a74cb29e4b5ec5184280e70
MrFobwatch <kcy0511@gmail.com>
2017-03-24 09:34:54 -0400

    Write Prototypes for myCalculator

--- Homework 3/myCalculator.cpp
+++ Homework 3/myCalculator.cpp
@@ -19,3 +19,39 @@ Calculator::Calculator() {
 double Calculator::add(double number) {
     
 }
+
+double Calculator::sub(double number) {
+    
+}
+
+double Calculator::mult(double number) {
+    
+}
+
+double Calculator::divd(double number) {
+    
+}
+
+double Calculator::getTotal() {
+    
+}
+
+void Calculator::clearTotal() {
+    
+}
+
+double Calculator::addM(double number) {
+    
+}
+
+double Calculator::subM(double number) {
+    
+}
+
+double Calculator::getMemTotal() {
+    
+}
+
+void Calculator::clearMem() {
+    
+}
